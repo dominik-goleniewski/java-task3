c8ac88a483a40c9e978f43a86d6c586498055640
Goleniewski <dgoleniewski@deloittece.com>
2022-07-18 15:48:09 +0200

    end

---
+++ src/Course.java
@@ -0,0 +1,11 @@
+public class Course {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+}
